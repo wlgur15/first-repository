6ec992c69bd6592d18ea24d2c33b4af6e8332f30
wlgur15 <rhwlgur3232@gmail.com>
2026-09-14 17:38:44 +0900

    hello man nice to meet you

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,10 @@
-def print_hello():
+def print_hello_world():
 	print("Hello World!!")
 
+def print_KIT():
+print("Kumoh National
+Institute of Technology.")
+
 if __name__ == '__main__':
-	print_hello()
+	print_KIT()
+	print_hello_world()
