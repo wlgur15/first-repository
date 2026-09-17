8d70d76abfde7bf60abded6333c2010bd02785ca
wlgur15 <rhwlgur3232@gmail.com>
2026-09-17 13:36:14 +0900

    Two files are changed

--- QuickSort.py
+++ QuickSort.py
@@ -23,7 +23,7 @@ def partition(a, pivot, high):
     return j
 
 
-a = [54,88,77,26,93,17,49,10,17,77,11,31,22,44,17,20]
+a = [54,88,78,26,94,17,49,16,13,73,11,31,22,44,17,23]
 print('Original list:\t', a)  
 qsort(a, 0, len(a)-1)
 print('Sorted list:\t', a)
--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import prime_number as pn
 
 if __name__ == '__main__':
     pn.is_prime_number(1)
-    pn.is_prime_number(13)
-    pn.is_prime_number(24)
-    pn.is_prime_number(31)
-    pn.is_prime_number(45)
-    pn.is_prime_number(97)
+    pn.is_prime_number(10)
+    pn.is_prime_number(21)
+    pn.is_prime_number(29)
+    pn.is_prime_number(32)
+    pn.is_prime_number(74)
